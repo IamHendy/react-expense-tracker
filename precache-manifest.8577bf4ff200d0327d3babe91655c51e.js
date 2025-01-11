bc0b1ba0349a2298c232486ce2861a18ddbfbeba
IamHendy <puchhendy@gmail.com>
2025-01-11 19:00:41 +0300

    Updates

---
+++ precache-manifest.8577bf4ff200d0327d3babe91655c51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019c9882348f9bd0895601e1a27c3d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dde46b0ec451abb9562f",
+    "url": "/static/css/main.69a4c9b2.chunk.css"
+  },
+  {
+    "revision": "aa4f755afde83eec6ff1",
+    "url": "/static/js/2.2de1baf2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.2de1baf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde46b0ec451abb9562f",
+    "url": "/static/js/main.660d4f71.chunk.js"
+  },
+  {
+    "revision": "7e9278031d01c1eb2cee",
+    "url": "/static/js/runtime-main.24f17132.js"
+  }
+]);
